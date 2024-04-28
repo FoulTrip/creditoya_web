8cdcdf9b213df9853b0c9c028df53a65613e11b6
foultrip <davidvasquezmahecha@gmail.com>
2024-04-27 19:46:27 -0500

    bug nodemailer fixed

--- src/app/lib/NodeMailer.ts
+++ src/app/lib/NodeMailer.ts
@@ -1,5 +1,5 @@
 import nodemailer from "nodemailer";
-import fs from "fs"
+import fs from "fs";
 
 const email = process.env.GOOGLE_EMAIL;
 const pass = process.env.GOOGLE_APP_KEY;
@@ -11,19 +11,3 @@ export const transporter = nodemailer.createTransport({
     pass,
   },
 });
-
-// Lee el archivo HTML con la plantilla de correo
-const readHTMLFile = (path, callback) => {
-  fs.readFile(path, { encoding: "utf-8" }, (err, html) => {
-    if (err) {
-      callback(err);
-    } else {
-      callback(null, html);
-    }
-  });
-};
-
-// export const mailOptions = {
-//     from: email,
-//     to:
-// }
